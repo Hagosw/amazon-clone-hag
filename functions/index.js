0fae9e277a81e18ed6dfabb5dc761f0d351f3864
Hagosw <noelh0618@gmail.com>
2024-12-09 11:05:49 -0500

    setting up Backend server

--- functions/index.js
+++ functions/index.js
@@ -1,46 +1,19 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-const express = require("express")
-const cors = require('cors');
-const { setGlobalOptions } = require("firebase-functions");
-const dotenv = require('dotenv').config()
 
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
 
-const stripe = require('stripe')(process.env.STRIPE_KEY)
-
-const app = express()
-setGlobalOptions({maxInstances: 10})
-app.use(cors({origin: true}))
-
-app.use(express.json())
-
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Success !"
-  })
-})
-
-app.post('/payment/create', async (req, res) => {
-  
-  const total = parseInt(req.query.total)
-  if (total > 0) {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: total,
-      currency: "usd",
-    })
-
-    // console.log(paymentIntent)
-
-    res.status(201).json({
-      clientSecret:paymentIntent.client_secret,
-    })
-  } else {
-    res.status(403).json({
-      message: "Total must be greater then 0"
-    })
-  }
-
-
-})
-
-exports.api = onRequest(app)
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
